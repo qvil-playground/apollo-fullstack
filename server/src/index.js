a6114d5404a1dd4c97cb73036fc2d9b3639df388
Taesu Hyeon <qvil1127@gmail.com>
2019-04-04 02:58:49 +0900

    3. Write your graph's resolvers
    - Run queries in the playground

--- server/src/index.js
+++ server/src/index.js
@@ -3,11 +3,13 @@ const typeDefs = require("./schema");
 const { createStore } = require("./utils");
 const LaunchAPI = require("./datasources/launch");
 const UserAPI = require("./datasources/user");
+const resolvers = require("./resolvers");
 
 const store = createStore();
 
 const server = new ApolloServer({
   typeDefs,
+  resolvers,
   dataSources: () => ({
     launchAPI: new LaunchAPI(),
     userAPI: new UserAPI({ store })
